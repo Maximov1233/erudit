dc5ecb47a3f2b64bb33a693933cd1ab27f40ee72
Maximov1233 <52001281+Maximov1233@users.noreply.github.com>
2020-04-19 21:41:43 +0600

    drag&drop

---
+++ src/letters.js
@@ -0,0 +1,167 @@
+export const alphabet = {
+    'A': {
+        symbol: 'А',
+        index: 10,
+        amount: 1
+    },
+    'Б': {
+        symbol: 'Б',
+        index: 3,
+        amount: 3
+    },
+    'В': {
+        symbol: 'В',
+        index: 5,
+        amount: 2
+    },
+    'Г': {
+        symbol: 'Г',
+        index: 3,
+        amount: 3
+    },
+    'Д': {
+        symbol: 'Д',
+        index: 5,
+        amount: 2
+    },
+    'Е': {
+        symbol: 'Е',
+        index: 9,
+        amount: 1
+    },
+    'Ё': {
+        symbol: 'Ё',
+        index: 9,
+        amount: 1
+    },
+    'Ж': {
+        symbol: 'Ж',
+        index: 2,
+        amount: 5
+    },
+    'З': {
+        symbol: 'З',
+        index: 2,
+        amount: 5
+    },
+    'И': {
+        symbol: 'И',
+        index: 8,
+        amount: 1
+    },
+    'Й': {
+        symbol: 'Й',
+        index: 4,
+        amount: 2
+    },
+    'К': {
+        symbol: 'К',
+        index: 6,
+        amount: 2
+    },
+    'Л': {
+        symbol: 'Л',
+        index: 4,
+        amount: 2
+    },
+    'М': {
+        symbol: 'М',
+        index: 5,
+        amount: 2
+    },
+    'Н': {
+        symbol: 'Н',
+        index: 8,
+        amount: 1
+    },
+    'О': {
+        symbol: 'О',
+        index: 10,
+        amount: 1
+    },
+    'П': {
+        symbol: 'П',
+        index: 6,
+        amount: 2
+    },
+    'Р': {
+        symbol: 'Р',
+        index: 6,
+        amount: 2
+    },
+    'С': {
+        symbol: 'С',
+        index: 6,
+        amount: 2
+    },
+    'Т': {
+        symbol: 'Т',
+        index: 5,
+        amount: 2
+    },
+    'У': {
+        symbol: 'У',
+        index: 3,
+        amount: 3
+    },
+    'Ф': {
+        symbol: 'Ф',
+        index: 1,
+        amount: 10
+    },
+    'Х': {
+        symbol: 'Х',
+        index: 2,
+        amount: 5
+    },
+    'Ц': {
+        symbol: 'Ц',
+        index: 1,
+        amount: 10
+    },
+    'Ч': {
+        symbol: 'Ч',
+        index: 2,
+        amount: 5
+    },
+    'Ш': {
+        symbol: 'Ш',
+        index: 1,
+        amount: 10
+    },
+    'Щ': {
+        symbol: 'Щ',
+        index: 1,
+        amount: 10
+    },
+    'Ъ': {
+        symbol: 'Ъ',
+        index: 1,
+        amount: 10
+    },
+    'Ы': {
+        symbol: 'Ы',
+        index: 10,
+        amount: 1
+    },
+    'Ь': {
+        symbol: 'Ь',
+        index: 2,
+        amount: 5
+    },
+    'Э': {
+        symbol: 'Э',
+        index: 2,
+        amount: 10
+    },
+    'Ю': {
+        symbol: 'Ю',
+        index: 1,
+        amount: 10
+    },
+    'Я': {
+        symbol: 'Я',
+        index: 3,
+        amount: 3
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {dictionary} from './dictionary.js';
+import {alphabet} from './letters.js';
 
 const box = document.querySelector('.box'),
 tripleWord = [0, 7, 14, 105, 119, 210, 224],
@@ -11,7 +12,7 @@ for (let i = 0; i < 225; i++) {
     boxCell.classList.add('box-cell');
     boxCell.dataset.pos = i + 1;
     if (i === 112) {
-        boxCell.innerHTML = 'CTR';
+        boxCell.innerHTML = 'ЦТР';
     }
     box.append(boxCell);
 }
@@ -19,18 +20,174 @@ for (let i = 0; i < 225; i++) {
 const boxCells = document.querySelectorAll('.box-cell');
 
 tripleWord.forEach((key) => {
-    boxCells[key].innerHTML = 'TW';
+    boxCells[key].innerHTML = 'ТС';
     boxCells[key].style.background = 'red';
 });
 doubleLetter.forEach((key) => {
-    boxCells[key].innerHTML = 'DL';
+    boxCells[key].innerHTML = 'ДБ';
     boxCells[key].style.background = '#00FFFF';
 });
 doubleWord.forEach((key) => {
-    boxCells[key].innerHTML = 'DW';
+    boxCells[key].innerHTML = 'ДС';
     boxCells[key].style.background = '#D2691E';
 });
 tripleLetter.forEach((key) => {
-    boxCells[key].innerHTML = 'TL';
+    boxCells[key].innerHTML = 'ТБ';
     boxCells[key].style.background = '#0000FF';
 });
+
+// let letter = document.createElement('div');
+// letter.classList.add('letter');
+// letter.setAttribute("draggable", "true");
+// letter.innerHTML = `
+//     <img src="./img/letter_bg.png">
+//     <p class="letter-symbol">${alphabet['Р'].symbol}</p>
+//     <p class="letter-index">${alphabet['Р'].index}</p>
+// `;
+
+
+const availableLetters = document.querySelector('.available-letters');
+
+for (let i = 0; i < 7; i++) {
+    const letter = document.createElement('div');
+    letter.classList.add('letter');
+    letter.setAttribute('draggable',"true");
+    letter.innerHTML = `
+        <img src="./img/letter_bg.png">
+        <p class="letter-symbol">${alphabet['Р'].symbol}</p>
+        <p class="letter-index">${alphabet['Р'].index} ${i}</p>
+`;
+availableLetters.append(letter);
+}
+
+// dragDrop
+
+const letters = document.querySelectorAll('.letter');
+
+// const dragDrop = () => {
+
+//     const dragStart = function () {
+//         setTimeout(() => {
+//             this.classList.add('hide');
+//         }, 0);
+//     };
+    
+//     const dragEnd = function () {
+//         this.classList.add('hide');
+//     };
+
+//     const dragOver = function (evt) {
+//         evt.preventDefault();
+//     };
+
+//     const dragEnter = function (evt) {
+//         evt.preventDefault();
+//         this.classList.add('hovered');
+//     };
+
+//     const dragLeave = function () {
+//         this.classList.remove('hovered');
+//     };
+
+//     const dragDrop = function () {
+//         console.log(this);
+//         this.append(letter);
+//         console.log(letter);
+//         this.classList.remove('hovered');
+//     };
+
+//     boxCells.forEach(cell => {
+//         cell.addEventListener('dragover', dragOver);
+//         cell.addEventListener('dragenter', dragEnter);
+//         cell.addEventListener('dragleave', dragLeave);
+//         cell.addEventListener('drop', dragDrop);
+//     });
+
+//     letters.forEach(letter => {
+//         letter.addEventListener('dragstart', dragStart);
+//         letter.addEventListener('dragend', dragEnd);
+//     });
+// }
+
+const dragStart = function (letter) {
+    setTimeout(() => {
+        letter.classList.add('hide');
+    }, 0);
+};
+
+const dragEnd = function (letter) {
+   letter.classList.remove('hide');
+};
+
+const dragOver = function (cell, evt) {
+    evt.preventDefault();
+};
+
+const dragEnter = function (cell, evt) {
+    evt.preventDefault();
+    cell.classList.add('hovered');
+};
+
+const dragLeave = function (cell) {
+    cell.classList.remove('hovered');
+};
+
+const dragDrop = function (cell, letter) {
+    cell.append(letter);
+    console.log(letter);
+    cell.classList.remove('hovered');
+};
+
+let letterUsed;
+
+letters.forEach(letter => {
+    
+    letter.addEventListener('dragstart', () => {
+        letterUsed = letter;
+        dragStart(letter);
+    });
+    letter.addEventListener('dragend', () => {
+        dragEnd(letter);
+    });
+});
+
+boxCells.forEach(cell => {
+    cell.addEventListener('dragover', () => {
+        dragOver(cell, event);
+    });
+    cell.addEventListener('dragenter',() => {
+        dragEnter(cell, event);
+    });
+    cell.addEventListener('dragleave', () => {
+        dragLeave(cell);
+    });
+    cell.addEventListener('drop', () => {
+        dragDrop(cell, letterUsed);
+    });     
+});
+
+// letters.forEach(letter => {
+//     letter.addEventListener('dragstart', dragStart);
+//     letter.addEventListener('dragend', dragEnd);
+// });
+
+// letters.forEach((letter) => {
+//     letter.addEventListener('dragstart', () => {
+//         dragDrop();
+//     });
+//     // letter.addEventListener('dragend', dragDrop);
+// });
+
+// dragDrop(letter);
+
+// boxCells.forEach(cell => {
+//     cell.addEventListener('click', () => {
+//         if (cell.hasChildNodes()) {
+//             cell.children[0].addEventListener('click', LetterDragAndDrop(cell.children[0]));
+//             console.log(cell.children[0]);
+//         }
+//     });
+// });
+
+
+
